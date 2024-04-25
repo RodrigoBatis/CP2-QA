c78f21ee265959392978d994cb204c5ee8ee57f9
RodrigoBatis <rodrigofreirebatista@gmail.com>
2024-04-25 19:04:41 -0300

    Project Base

---
+++ src/test/java/org/estudos/br/ConsultaIBGETest.java
@@ -0,0 +1,52 @@
+package org.estudos.br;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ConsultaIBGETest {
+    private static final String ESTADOS_API_URL = "https://servicodados.ibge.gov.br/api/v1/localidades/estados/";
+    private static final String DISTRITOS_API_URL = "https://servicodados.ibge.gov.br/api/v1/localidades/distritos/";
+
+    @Test
+    @DisplayName("Teste para consulta única de um estado")
+    public void testConsultarEstado() throws IOException {
+        // Arrange
+        String uf = "SP"; // Define o estado a ser consultado
+
+        // Act
+        String resposta = ConsultaIBGE.consultarEstado(uf); // Chama o método a ser testado
+
+        // Assert
+        // Verifica se a resposta não está vazia
+        assert !resposta.isEmpty();
+
+        // Verifica se o status code é 200 (OK)
+        HttpURLConnection connection = (HttpURLConnection) new URL(ESTADOS_API_URL + uf).openConnection();
+        int statusCode = connection.getResponseCode();
+        assertEquals(200, statusCode, "O status code da resposta da API deve ser 200 (OK)");
+    }
+
+    @Test
+    @DisplayName("Teste para consulta única de um distrito")
+    public void testConsultarDistrito() throws IOException {
+        // Arrange
+        int id = 160030312 ; // Define o estado a ser consultado
+
+        // Act
+        String resposta = ConsultaIBGE.consultarDistrito(id); // Chama o método a ser testado
+
+        // Assert
+        // Verifica se a resposta não está vazia
+        assert !resposta.isEmpty();
+
+        // Verifica se o status code é 200 (OK)
+        HttpURLConnection connection = (HttpURLConnection) new URL(DISTRITOS_API_URL + id).openConnection();
+        int statusCode = connection.getResponseCode();
+        assertEquals(200, statusCode, "O status code da resposta da API deve ser 200 (OK)");
+    }
+}
